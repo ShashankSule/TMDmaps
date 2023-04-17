9ea3f2006f549bb59979c3e2e21950931ee1a6ee
ShashankSule <Shashank Sule>
2023-04-17 18:49:43 -0400

    added biased sampling mode to 1d simulation script, tuned epsilon range

--- run/1d_bias_error.py
+++ run/1d_bias_error.py
@@ -42,12 +42,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--sample", type=str, help="type of sampling density", default="uniform")
 parser.add_argument("--func", type=str, help="Function whose Lf(0) we shall approx", default="committor")
 parser.add_argument("--parallel", type=bool, help="Flag to compute in parallel or not", default=False)
+parser.add_argument("--note", type=str, help="Any additional notes", default="")
 args = parser.parse_args()
 
 # set regime 
-sampling = args.sample
+sample = args.sample
 func = args.func
 parallel = args.parallel
+note = args.note
 
 # set up params
 beta = 1
@@ -74,15 +76,17 @@ def committor(x):
     else: 
         return (1/Z_committor)*scint.quad(lambda y: np.exp(beta*potential(y)), -0.9,x)[0]
 
-def task(t, regime="uniform"):
+def task(t, regime="uniform", func="committor"):
     # ϵ_uniform = epsilons_uniform[i]
     np.random.seed() 
     ϵ, _  = t
-    N = int(ϵ**(-3))
     
+    # sample based on regime 
     if regime=="uniform":
+        N = int(ϵ**(-3))
         data = np.random.uniform(-2.0,2.0,N+1).reshape(N+1,d)
     else:
+        N = int(ϵ**(-5))
         data = np.random.randn(int(N+1)).reshape(N+1,d)
     
     data[N,0] = 0.0
@@ -96,43 +100,55 @@ def task(t, regime="uniform"):
     target_dmap.construct_generator(data.T)
     # print("Got kernel!")
     K = target_dmap.get_kernel()
-    L = target_dmap.get_generator() 
-    # check committor on uniform set 
-    committor_data = np.apply_along_axis(committor, 1, data)
-    # LF_uniform_true = Lf(uniform)
-    Lcommittor_atzero = L[-1,:]@committor_data
-    print("Got result!")
-    return Lcommittor_atzero[0]
+    L = target_dmap.get_generator()
 
-if sampling=="uniform":
+    # approx. based on function  
+    if func=="committor":
+        F = np.apply_along_axis(committor, 1, data) 
+        Lf_atzero_TMD = L[-1,:]@F
+        ans = Lf_atzero_TMD[0] 
+    else: 
+        F = np.apply_along_axis(f,1,data)
+        Lf_atzero_TMD = L[-1,:]@F
+        ans = Lf_atzero_TMD[0] - Lf(0.0)
+    
+    print("Got result!")
+    return ans
+
+if sample=="uniform":
     # set up info 
     epsilons = np.linspace(0.04, 0.06, 10)  # actual sim 
     # epsilons = np.linspace(0.06, 0.07, 2)     # trial params for debug 
-    Ns_uniform = epsilons**(-3)
-    epsilons_range = len(epsilons)
-    ntrials = 12 # actual sim 
-    # ntrials = 1    # trial params for debug 
-    trial_ids = np.linspace(1,ntrials,ntrials)
-    Lcommittor_uniform_TMD = np.zeros((epsilons_range, ntrials))
-    
-    if parallel:
-        for i in tqdm.tqdm(range(epsilons_range)):
-            print("Starting new epsilon...")
-            ϵ = epsilons[i]
-            def task_sub(x): return task([ϵ,x], regime="uniform")
-            with multiprocess.Pool(2) as pool: 
-                result = pool.map(task_sub, list(trial_ids))
-            Lcommittor_uniform_TMD[i,:] = np.array(result)
-    else:
-        for i in tqdm.tqdm(range(epsilons_range)):
-            ϵ = epsilons[i]
-            def task_sub(x): return task([ϵ,x], regime="uniform")
-            for j in tqdm.tqdm(range(ntrials)):
-                Lcommittor_uniform_TMD[i,j] = task_sub(trial_ids[j])
-
-    # save sim 
-    filename = sampling + '_' + func + '_' + str(datetime.datetime.now())
-    np.save(os.getcwd()+'/run/'+filename, Lcommittor_uniform_TMD) 
 else: 
-    print("biased mode not written yet!")
+    # epsilons = np.linspace(0.14, 0.18, 10) # actual sim
+    epsilons = np.linspace(0.16,0.17,2) # trial params for debug  
+    
+
+
+epsilons_range = len(epsilons)
+ntrials = 12 # actual sim 
+# ntrials = 1    # trial params for debug 
+trial_ids = np.linspace(1,ntrials,ntrials)
+Lpointwise_errors_TMD = np.zeros((epsilons_range, ntrials))
+
+# now compute! 
+if parallel:
+    for i in tqdm.tqdm(range(epsilons_range)):
+        print("Starting new epsilon...")
+        ϵ = epsilons[i]
+        def task_sub(x): return task([ϵ,x], regime=sample,func=func)
+        with multiprocess.Pool(2) as pool: 
+            result = pool.map(task_sub, list(trial_ids))
+        Lpointwise_errors_TMD[i,:] = np.array(result)
+else:
+    for i in tqdm.tqdm(range(epsilons_range)):
+        ϵ = epsilons[i]
+        def task_sub(x): return task([ϵ,x], regime=sample, func=func)
+        for j in tqdm.tqdm(range(ntrials)):
+            Lpointwise_errors_TMD[i,j] = task_sub(trial_ids[j])
+
+# save sim 
+stats = {'epsilons': epsilons, 'pointwise_errors': Lpointwise_errors_TMD}
+filename = sample + '_' + func + '_' + note + '_' + str(datetime.datetime.now())
+np.save(os.getcwd()+'/run/'+filename, stats) 
 
