6f550a462f67522b7188eaeb64b2667ebff3b95d
Shashank Sule <shashanksule8@gmail.com>
2023-04-23 17:36:38 -0400

    Update 1d_sim.sh
    Tweaked bash script to include parallel flag.

--- 1d_sim.sh
+++ 1d_sim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python3 run/1d_bias_error.py --sample uniform --func committor --note fixedN 
-python3 run/1d_bias_error.py --sample biased --func committor --note fixedN
-python3 run/1d_bias_error.py --sample uniform --func trigpoly --note fixedN 
-python3 run/1d_bias_error.py --sample biased --func trigpoly --note fixedN
+python3 run/1d_bias_error.py --sample uniform --func committor --parallel True --note fixedN 
+python3 run/1d_bias_error.py --sample biased --func committor --parallel True --note fixedN
+python3 run/1d_bias_error.py --sample uniform --func trigpoly --parallel True --note fixedN 
+python3 run/1d_bias_error.py --sample biased --func trigpoly --parallel True --note fixedN
