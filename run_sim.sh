f32c19a45bcce1c70d0ec26c524cce3f32386ef6
Shashank Sule <shashanksule8@gmail.com>
2023-07-14 19:35:37 -0400

    Twowell simulation

--- run_sim.sh
+++ run_sim.sh
@@ -2,14 +2,14 @@
 
 rootpath=$(pwd)
 
-# Muller
-fempath=$rootpath/data/Muller/ground_data/DistmeshMuller_20.mat 
-savepath=$rootpath/data/Muller/error_data/
+# # Muller
+# fempath=$rootpath/data/Muller/ground_data/DistmeshMuller_20.mat 
+# savepath=$rootpath/data/Muller/error_data/
 
-# # Twowell 
-# fempath=$rootpath/data/Twowell/ground_data/DistmeshTwowell_1.mat 
-# savepath=$rootpath/data/Twowell/error_data 
+# Twowell 
+fempath=$rootpath/data/Twowell/ground_data/DistmeshTwowell_1.mat 
+savepath=$rootpath/data/Twowell/error_data 
 
 for var in uniform 
-    do python3 src/error_analysis.py --sys muller --dset $var --tru $fempath --save $savepath 
+    do python3 src/error_analysis.py --sys twowell --dset $var --tru $fempath --save $savepath 
 done
